04a1f0d0abf909be33bb52ca85c62544887ea46a
trangking <trangking1@hotmail.com>
2024-11-15 16:25:08 +0700

    ---

--- src/Components/Login/LoadingLIne.jsx
+++ src/Components/Login/LoadingLIne.jsx
@@ -48,7 +48,7 @@ export default function LoadingLine() {
           navigate("/member");
         });
       } else {
-                // ส่งกลับหน้าเดิม
+        // ส่งกลับหน้าเดิม
         navigate("/");
         localStorage.clear();
         message.error("ท่านยังไม่ได้ลงทะเบียน Line กรุณาติดต่อพนักงาน");
